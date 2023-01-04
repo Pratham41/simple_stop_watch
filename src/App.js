c4cb17bf3c5203765d93a190d5876e0cda8e45e6
Pratham41 <kamnleprathmesh41@gmail.com>
2023-01-04 20:55:01 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [startClicked, setStartClicked] = useState(false);
+  const [time, setTime] = useState(0);
+
+  const startButtonClickHandler = () => {
+    setStartClicked(!startClicked);
+  };
+
+  const resetButtonClickHandler = () => {
+    setTime(0);
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (startClicked) {
+      interval = setInterval(() => {
+        setTime((time) => time + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [startClicked]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+        <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </h1>
+      <button style={{ backgroundColor: `${startClicked ? 'red' : 'green'}` }} onClick={startButtonClickHandler}>
+        {startClicked ? "Pause" : "Start"}
+      </button>
+      <button style={{ backgroundColor: 'yellow' }} onClick={resetButtonClickHandler}>Reset</button>
     </div>
   );
 }
